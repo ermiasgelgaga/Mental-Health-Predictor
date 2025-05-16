69de13875e3926681895f1334524a63c95da515a
ermiasgelgaga <ermiasgelgaga@gmail.com>
2025-05-16 09:41:12 -0400

    app.py

--- app.py
+++ app.py
@@ -7,36 +7,40 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 import pandas.io.sql as pdsql
-from config import pg_user, pg_password, db_name
+from config import pg_user, pg_password, db_name, pg_host  # Corrected import
 from flask import Flask, jsonify, render_template, abort, redirect
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, MetaData
 
+from flask import Flask, jsonify, render_template
+import pandas as pd
+import json
+from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
-# Load env variables
 load_dotenv()
-
-# Fetch from environment
-pg_user = os.getenv("PG_USER")
-pg_password = os.getenv("PG_PASSWORD")
-pg_host = os.getenv("PG_HOST")
-pg_port = os.getenv("PG_PORT", "5432")  # default port
-db_name = os.getenv("DB_NAME")
-
-
 
 #################################################
 # Database Setup
 ##################################################
 
-# Construct the DATABASE_URL
+pg_user = os.getenv("PG_USER")
+pg_password = os.getenv("PG_PASSWORD")
+pg_host = os.getenv("PG_HOST")
+pg_port = os.getenv("PG_PORT", "5432")
+db_name = os.getenv("DB_NAME")
+
+
 DATABASE_URL = f"postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{db_name}?sslmode=require"
 DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://', 1)
 
+engine = create_engine(DATABASE_URL)
+meta = sqlalchemy.MetaData()
+meta.reflect(bind=engine)
+table_names = meta.tables.keys()
+print(table_names)  # or use the list of table_names as needed
 
 
-# checking the table names
-engine.table_names()
 
 
 #################################################
@@ -44,11 +48,11 @@ engine.table_names()
 #################################################
 app = Flask(__name__)
 
-app.config['SQLAlCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URI','') or "sqlite:///db.sqlite"
+# Fix the typo in the configuration keys
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', '') or "sqlite:///db.sqlite"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.config['SQLAlCHEMY_TRACK_MODIFICATION']=False
-
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 #################################################
 # Flask Routes
@@ -57,14 +61,24 @@ db=SQLAlchemy(app)
 def index():
     return render_template("index.html")
 
-
-@app.route("/api")
-def searchbyyear():
+@app.route("/mental_health")
+def mental_health():
     sqlStatement = """
     SELECT * FROM mental_health;
     """
     df = pdsql.read_sql(sqlStatement, engine)
     df.set_index('Timestamp', inplace=True)
+    df = df.to_json(orient='table')
+    result = json.loads(df)
+    return jsonify(result)
+
+@app.route("/indicator")
+def indicator():
+    sqlStatement = """
+    SELECT * FROM development_indicator;
+    """
+    df = pdsql.read_sql(sqlStatement, engine)
+    df.set_index('country_name', inplace=True)
     df = df.to_json(orient='table')
     result = json.loads(df)
     return jsonify(result)
